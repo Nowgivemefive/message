dbefad913e5f9194cc839dde40eb4254bec3b0e8
zhaoyuan <786211677@qq.com>
2017-06-04 23:58:14 +0800

    评论功能添加中还没添加

--- php/register.php
+++ php/register.php
@@ -19,6 +19,11 @@ if (mysqli_query($conn, $sql)) {
 	$_SESSION['name'] = $name;
 	$_SESSION['sex'] = $sex;
 	$_SESSION['conn'] = $conn;
+	$last_login_ip = $_SERVER['REMOTE_ADDR'];
+	$last_login_time = date("Y-m-d H:i:s");
+	$update_user_sql = "UPDATE infomation SET last_login_ip = '".$last_login_ip
+	."', last_login_time= '".$last_login_time."' WHERE uid = ".$_SESSION['uid'];
+	mysqli_query($conn,$update_user_sql);
 	header("location:../index.php");
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
